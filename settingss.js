be3396a3e5039beaaaa7caa0cdc3874809bec47d
shadrachgraham29-ship-it <shadrachgraham29@gmail.com>
2025-08-17 17:53:01 +0000

    Update settingss.js

--- settingss.js
+++ settingss.js
@@ -1,8 +1,8 @@
 
 module.exports = {
-  SESSION_ID: "",  // add your Session Id here
+  SESSION_ID: "malvin~szkAwLAK#woW3yvTEpc6RFu7Djlwm-ZN1TPEQMJ5UrfnpdIDbacA",  // add your Session Id here
   
-  OWNER_NUMBER: "263714757857", // put your phone number here
+  OWNER_NUMBER: "233558068682", // put your phone number here
   
   PREFIX: ".", // prefix (e.g., ., /, !, *)
   
